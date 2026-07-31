0dc02839b2918f02ac1b8e02700b8320e173c4f5
Manuela Greimel <originalkittycoffeeshop@yahoo.com>
2026-07-31 21:47:11 +0200

    first bit of data, model + controller

---
+++ Controllers/BookController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        static List<Book> books = new List<Book> {
+            new Book { Id = 1, Title = "Intermezzo", Author = "Sally Rooney", Genre = "Novel" },
+            new Book { Id = 2, Title = "The Hundred Years' War On Palestine", Author = "Rashid Khalidi", Genre = "History" },
+            new Book { Id = 3, Title = "The vegetarian", Author = "Han Kang", Genre = "Novel" }
+        };
+
+        [HttpGet]
+        public async Task<ActionResult<List<Book>>> GetBooks()
+            => await Task.FromResult(Ok(books));
+    }
+}
---
+++ Models/Book.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApi.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Author { get; set; } = string.Empty;
+        public string Genre {  get; set; } = string.Empty;
+    }
+}
+
+// adjust to external API
